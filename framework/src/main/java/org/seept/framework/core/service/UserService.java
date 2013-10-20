541b50f2fd5c81cd208e2ede9c13f4eaba404bf5
quanet <quanet@163.com>
2013-10-20 17:13:08 +0800

    暂时移除Modules模块,并让其以依赖包的形式存在

--- framework/src/main/java/org/seept/framework/core/service/UserService.java
+++ framework/src/main/java/org/seept/framework/core/service/UserService.java
@@ -6,10 +6,15 @@ import org.seept.framework.core.util.QueryUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author : lihaoquan
@@ -47,6 +52,34 @@ public class UserService {
         return (List<User>) userDao.findAll();
     }
 
+    /**
+     * 获取分页处理的用户列表
+     * @param filterParmatersMap
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    public Page<User> getPageUsers(Map<String,Object> filterParmatersMap,int pageNum,int pageSize) {
+        return this.getPageUsers(filterParmatersMap, pageNum, pageSize,null);
+    }
+
+    /**
+     *获取分页处理的用户排序列表
+     *
+     * 需要一个 PageRequest 和 Specification的 组合
+     *
+     * @param filterParmatersMap
+     * @param pageNum
+     * @param pageSize
+     * @param sortType
+     * @return
+     */
+    public Page<User> getPageUsers(Map<String,Object> filterParmatersMap,int pageNum,int pageSize,String sortType) {
+        PageRequest pageRequest = buildPageRequest(pageNum,pageSize,sortType);
+        Specification<User> specification = buildSpecification(filterParmatersMap);
+        return userDao.findAll(specification,pageRequest);
+    }
+
     public User getUser(String id) {
         return userDao.findById(id);
     }
@@ -66,6 +99,35 @@ public class UserService {
      */
     public void deleteUser(String id) {
         userDao.delete(id);
+    }
+
+
+    /**
+     * 创建分页请求
+     * @param pageNum
+     * @param pageSize
+     * @param sortType
+     * @return
+     */
+    public PageRequest buildPageRequest(int pageNum,int pageSize,String sortType) {
+        Sort sort = null;
+        if("auto".equals(sortType)) {
+            sort = new Sort(Sort.Direction.DESC,"id");
+        }else if("title".equals(sortType)) {
+            sort = new Sort(Sort.Direction.ASC,"loginName");
+        }
+        return new PageRequest(pageNum-1,pageSize,sort);
+    }
+
+    /**
+     *
+     * @param parmatersMap
+     * @return
+     */
+    public Specification<User> buildSpecification(Map<String,Object> parmatersMap) {
+        Specification<User> specification = null;
+
+        return specification;
     }
 
 }
