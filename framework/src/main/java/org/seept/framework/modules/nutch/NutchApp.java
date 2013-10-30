d83dc9a84e9bd4107c959d361a5573b7e6386865
lihaoquan <quanet@163.com>
2013-10-30 11:06:28 +0800

    调整了目录结构

---
+++ framework/src/main/java/org/seept/framework/modules/nutch/NutchApp.java
@@ -0,0 +1,12 @@
+package org.seept.framework.modules.nutch;
+
+/**
+ * @author: Quan
+ * @Description:
+ */
+public class NutchApp {
+
+    public static void main(String[] args) {
+        System.out.println(".............Nutch Æô¶¯");
+    }
+}
--- framework/src/main/java/org/seept/framework/modules/search/App.java
+++
@@ -1,9 +0,0 @@
-package org.seept.framework.modules.search;
-
-/**
- * @author : lihaoquan
- *
- * 搜索模块
- */
-public class App {
-}
