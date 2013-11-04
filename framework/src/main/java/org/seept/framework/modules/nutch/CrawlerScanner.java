1ba09a7dd102229ab795cf224a93a84260c21d82
lihaoquan <quanet@163.com>
2013-11-04 15:29:15 +0800

    为爬虫扫描器提供了JDK-TIMER的调度支持

---
+++ framework/src/main/java/org/seept/framework/modules/nutch/CrawlerScanner.java
@@ -0,0 +1,27 @@
+package org.seept.framework.modules.nutch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: Quan
+ * @Description: 爬虫扫描器
+ */
+@Component
+public class CrawlerScanner {
+
+    private static Logger logger = LoggerFactory.getLogger(CrawlerScanner.class);
+
+    /**
+     * 通过JDK的方式执行
+     */
+    public void executeByJdk() {
+        execute("jdk basic job");
+    }
+
+    private void execute(String by) {
+        long count = 80;
+        logger.info("There are {} crawlers running now , printed by {}.", count, by);
+    }
+}
