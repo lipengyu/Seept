7fc6d282290fdbd53f4dc6e87f64748e063a7c6c
lihaoquan <quanet@163.com>
2013-11-05 17:04:54 +0800

    尝试了注册请求的Rest风格请求

--- framework/src/main/java/org/seept/framework/core/web/user/RegisterController.java
+++ framework/src/main/java/org/seept/framework/core/web/user/RegisterController.java
@@ -3,6 +3,7 @@ package org.seept.framework.core.web.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,6 +23,13 @@ public class RegisterController {
     @RequestMapping(method = RequestMethod.GET)
     public String registerPage() {
         logger.info("前往进行注册申请页面!");
+        return "/account/register.jsp";
+    }
+
+
+    @RequestMapping(value = "/{id}")
+    public String check(@PathVariable("id") String id) {
+        logger.info("前往进行注册申请页面!(check):"+id);
         return "/account/register.jsp";
     }
 }
