e48b4eab8b44b0620bec921895a70775b7872f85
lihaoquan <quanet@163.com>
2013-11-06 14:52:42 +0800

    在项目中引入CXF maven 配置

---
+++ framework/src/main/java/org/seept/framework/modules/webservice/jaxrs/UserJaxRsService.java
@@ -0,0 +1,10 @@
+package org.seept.framework.modules.webservice.jaxrs;
+
+/**
+ * @author: Quan
+ * @Description:
+ */
+public class UserJaxRsService {
+
+
+}
---
+++ framework/src/main/java/org/seept/framework/modules/webservice/rest/UserRestController.java
@@ -0,0 +1,8 @@
+package org.seept.framework.modules.webservice.rest;
+
+/**
+ * @author: Quan
+ * @Description:
+ */
+public class UserRestController {
+}
